ba3fd291126eb5ad1de5639e31d391b477634c46
jordast <twilight.rvn@gmail.com>
2014-03-13 23:09:28 +0100

    Update fibonacci.cs

--- fibonacci.cs
+++ fibonacci.cs
@@ -17,15 +17,23 @@ namespace Fibunacci
             InitializeComponent();
         }
 
+
+        // --- btnCalculate_Click ---
+        //
+        // Hier wird die Ermittlung der Fibunacci Werte bis zu einer bestimmten 
+        // Position in der Folge gestartet.
+        // Die gesuchte Position wird über die Oberfläche eingegeben.
+        // Die Ausgabe der ermittelten Werte erfolgt in einer Listbox
+        //
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            Int32 fibCounter = 0;
+            Int32 position = 0;
             
             lbxResult.Items.Clear();
 
             try
             {
-                fibCounter = Convert.ToInt32(txtCount.Text);
+                position = Convert.ToInt32(txtPosition.Text);
             }
             catch (FormatException ex)
             {
@@ -38,7 +46,7 @@ namespace Fibunacci
             
             try
             {
-                for (int i = 1; i <= fibCounter; i++)
+                for (int i = 1; i <= position; i++)
                 {
                     lbxResult.Items.Add(fibunacci(i));
                 }    
@@ -50,6 +58,21 @@ namespace Fibunacci
                   
         }
         
+        // --- fibunacci ---
+        //
+        // Hier wird anhand der Übergebenen Position die entsprechende Zahl der 
+        // Fibunacci-Folge ermittelt und zurück gegeben.
+        // Die Ermittlung erfolgt durch den Rekursiven aufruf wie diser in der
+        // Funktion der Fibunacci Folge festgelegt ist:
+        //
+        // f(n) = f(n-1) + f(n-2) 
+        //
+        // Das heißt der Gesuchte Wert ist die Summe der beiden vorherigen Werte.
+        //
+        // Da bei diesem Rekursiven Aufruf sämmtliche Vorgängerwerte ermittelt werden
+        // müssen dauert die Ermittlung des Wertes um ein vielfaches länger, je höher
+        // die gesuchte Stelle ist.
+        //
         private Int32 fibunacci(Int32 value)
         {
             if (value == 1 || value == 2) return 1;
